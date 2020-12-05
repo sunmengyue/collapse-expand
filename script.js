1775eabd271ad57ff4f8d4c5a67f241e28c1d9dd
Simon <bardrabbit@gmail.com>
2020-12-05 16:49:11 -0500

    traverse from the document element

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-const buttons = document.querySelectorAll('.expand-button');
-
-buttons.forEach((b) =>
-  b.addEventListener('click', (e) => {
-    // 1. get the card that being clicked
-    const parent = e.target.closest('.card');
-    parent.dataset.id = new Date().valueOf().toString();
-    const cardBody = parent.children[1];
-    // 2. Expand or collapse based on the card status
-    if (cardBody.classList.contains('show')) {
-      //button text change to collapse
-      b.innerText = 'Expand';
-      //remove the show class
-      cardBody.classList.remove('show');
-    } else {
-      b.innerText = 'Collapse';
-      cardBody.classList.add('show');
-    }
-  }),
-);
+document.addEventListener('click', (e) => {
+  if (!e.target.matches('.expand-button')) return;
+  const card = e.target.closest('.card');
+  const cardBody = card.querySelector('.card-body');
+  cardBody.classList.toggle('show');
+  e.target.innerText = e.target.innerText === 'Expand' ? 'Collapse' : 'Expand';
+});
